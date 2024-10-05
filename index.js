0e1e1f6b297fce204007ed416ea478efaf5d57d7
Jeremias Vivas <notjxreeh26@gmail.com>
2024-10-05 10:52:53 -0300

    Repo base

---
+++ index.js
@@ -0,0 +1,27 @@
+const buttonToUp = document.getElementById("buttonUp");
+
+function goUp() {
+  window.addEventListener("scroll", () => {
+    var scroll = document.documentElement.scrollTop;
+    if (scroll > 400) {
+      buttonToUp.style.opacity = 100;
+      buttonToUp.style.right = 5 + "rem";
+    } else {
+      buttonToUp.style.right = -10 + "rem";
+      buttonToUp.style.opacity = 0;
+    }
+  });
+}
+
+function clickToUp() {
+  const buttonToUp = document.getElementById("buttonUp");
+  buttonToUp.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+}
+
+clickToUp();
+goUp();
